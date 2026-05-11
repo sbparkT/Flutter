6011dca21df2a3a47b3d18d494e1ad3046822fe5
sbparkT <sbpark97@daehyun.hs.kr>
2026-05-11 22:54:31 +0900

    Create dart_basic_03.dart

---
+++ dart_basic_03.dart
@@ -0,0 +1,14 @@
+  void main() {
+    //어떤 Type이든 뭐든 다 넣을 수 있는 dynamic
+    dynamic name='짱구';
+    print(name);
+    dynamic number=1;
+    print(number);
+    var name2='철수';
+    print(name2);
+    print(name.runtimeType);
+    print(name2.runtimeType);
+    name=true; //dynamic은 type 변형 가능
+    //name2=true; //error :var은 type 변형 불가 
+    print(name);
+  }
