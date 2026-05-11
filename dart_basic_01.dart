19983e895e95a29a95976696189ecc23a1d67d71
sbparkT <sbpark97@daehyun.hs.kr>
2026-05-11 22:05:43 +0900

    Create dart_basic_01.dart

---
+++ dart_basic_01.dart
@@ -0,0 +1,32 @@
+  void main() {
+  //variable 변수 
+    var name="코드팩터리";
+    print(name);
+    var name2="레드벨벳";
+    print(name2);
+    name="hi";
+    print("name");
+    //var name="hi"; //동일한 변수이름사용가능 
+    
+    int num1=2;//정수
+    print(num1);
+    int num2=4;
+    print(num2);
+    print(num1+num2);
+    print(num1-num2);
+    print(num1*num2);
+    print(num1/num2);
+    
+    double num3=2.5;
+    double num4=0.5;
+    print(num3+num4);      
+    print(num3-num4);      
+    print(num3*num4);      
+    print(num3/num4); 
+    
+    bool isTrue=true;
+    bool isFalse=false;
+    print(isTrue);
+    print(isFalse);
+    
+  }
